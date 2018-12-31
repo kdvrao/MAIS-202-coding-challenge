0907e29f6712ad9aa23d847714be6133c309c421
Kiren Rao <kiren_rao@yahoo.com>
2018-12-31 13:07:59 -0600

    Added working code and updated readme

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # MAIS-202-coding-challenge
+
+In order to run the code simply type the following into the terminal
+
+```
+python graph.py
+```
+This will display the graph and the average values in the terminal.
+
+If you would like to only see the average values in order to valudate the results, simply enter:
+
+```
+python graph.py table
+```
+Simmilary, if you only want the graph:
+
+```
+python graph.py graph
+```
---
+++ graph.py
@@ -0,0 +1,33 @@
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt
+import sys
+
+if len(sys.argv) > 1:
+    todo = sys.argv[1]
+else:
+    todo = "both"
+
+
+#parse csv file
+dataset = pd.read_csv("data.csv") 
+#print(dataset.describe())
+
+#create a subset and preform operations on it as descripbed in the problem
+#i.e get what we need
+#get the mean of the dataset based on the group we made before
+purpose_mean = dataset.groupby("purpose").mean()[["int_rate"]]
+
+# Plot 
+stack_bar_plot = purpose_mean.unstack().plot(kind='bar',title="Mean Intrest Rate Per Purpose",figsize=(8, 4))
+stack_bar_plot.set_xlabel(purpose_mean.index.name)
+stack_bar_plot.set_ylabel("mean(int_rate)")
+#plt.show()
+
+if todo == "graph":
+  plt.show()
+elif todo == "table":
+  print(purpose_mean)
+else:
+  print(purpose_mean)
+  plt.show()
